5a2159119560bda8325caf650938d1cf0da935ce
kifayatullah03 <kifayat.amini001@gmail.com>
2022-08-28 18:56:54 +0500

    evenNumber updated

--- src/Example01/NumbersEx.java
+++ src/Example01/NumbersEx.java
@@ -3,8 +3,9 @@ package Example01;
 public class NumbersEx {
     public static void main(String[] args) {
 
-        for (int i = 1; i <100 ; i+=2) {
+        for (int i = 2; i <100 ; i+=2) {
             System.out.println(i);
         }
+        System.out.println("the even number updated");
     }
 }
